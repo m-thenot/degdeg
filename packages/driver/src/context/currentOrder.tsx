ce3962b798437fc24f32484e966036c3c62ecf47
MartinT <martinthenot@gmail.com>
2022-02-13 13:36:44 +0100

    feat(driver): save current order

--- packages/driver/src/context/currentOrder.tsx
+++ packages/driver/src/context/currentOrder.tsx
@@ -2,7 +2,13 @@ import React, { createContext, useEffect } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import { IOrder, OrderStatus } from '@dagdag/common/types';
 import { currentOrderState, ordersState } from '@stores/orders.atom';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import {
+  getObjectData,
+  removeData,
+  storeObjectData,
+} from '@dagdag/common/utils';
+import { isOnlineState } from '@stores/driver.atom';
 
 interface IContextProps {
   currentOrder: IOrder | null;
@@ -13,16 +19,46 @@ const CurrentOrderContext = createContext<Partial<IContextProps>>({});
 const CurrentOrderProvider: React.FC = ({ children }) => {
   const [orders, setOrders] = useRecoilState(ordersState);
   const currentOrder = useRecoilValue(currentOrderState);
+  const setIsOnline = useSetRecoilState(isOnlineState);
 
   useEffect(() => {
+    const getSavedOrder = async () => {
+      const savedOrder = await getObjectData('currentOrder');
+      if (savedOrder) {
+        setIsOnline(true);
+        const newOrders = [JSON.parse(savedOrder)];
+        setOrders(newOrders);
+      }
+    };
+
+    getSavedOrder();
+  }, []);
+
+  useEffect(() => {
+    const clearOrder = async () => {
+      await removeData('currentOrder');
+    };
+
     if (currentOrder?.status === OrderStatus.CANCELED_BY_DRIVER) {
       const newOrders = [...orders];
       newOrders.shift();
       setOrders(newOrders);
     }
+    if (
+      currentOrder?.status === OrderStatus.FINISHED ||
+      currentOrder?.status?.startsWith('CANCEL')
+    ) {
+      clearOrder();
+    }
   }, [currentOrder]);
 
   useEffect(() => {
+    const saveOrder = async (order: IOrder) => {
+      await storeObjectData(order, 'currentOrder');
+    };
+    if (currentOrder) {
+      saveOrder(currentOrder);
+    }
     const orderSubscriber = firestore()
       .collection('orders')
       .doc(currentOrder?.uid)
